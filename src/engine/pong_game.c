c7d07c648f30b417245c2769d555f2d6a10e61ac
monoamine11231 <monoamine11231@gmail.com>
2023-11-12 22:25:00 +0100

    Two player mode and AI modes I/O correctly implemented

--- src/engine/pong_game.c
+++ src/engine/pong_game.c
@@ -45,6 +45,28 @@ void render_pong_game(enum engine_state *state) {
     struct ball ball;
     
     int score = 0;
+    int ai_mode = 0;
+    ai_mode |= (*state==GAME_START_AI_EASY);
+    ai_mode |= (*state==GAME_START_AI_MEDIUM);
+    ai_mode |= (*state==GAME_START_AI_HARD);
+    
+    /* How fast will the AI board respond to the ball's y axis change? */
+    float ai_response = 0.0f;
+    switch (*state)
+    {
+    case GAME_START_AI_EASY:
+        ai_response = 0.3f;
+        break;
+    case GAME_START_AI_MEDIUM:
+        ai_response = 0.5f;
+        break;
+    case GAME_START_AI_HARD:
+        ai_response = 0.7f;
+        break;
+    default:
+        break;
+    }
+
 
     boards[0].x = 0;
     boards[0].y = 0;
@@ -65,12 +87,22 @@ void render_pong_game(enum engine_state *state) {
         clear_video();
         source_io_shield_inputs(&st);
 
-        /* Control so that the boards are not going out of the screen */
-        boards[0].y = MIN(boards[0].y+st.btn1, 32-st.btn1-boards[0].width);
-        boards[0].y = MAX(boards[0].y-st.btn2, 0);
+        if (ai_mode) {
+            boards[1].y += (int)((ball.y-(float)(boards[1].y))*ai_response);
+            boards[1].y = MIN(boards[1].y, 32-boards[1].width);
+            boards[1].y = MAX(boards[1].y, 0);
 
-        boards[1].y = MIN(boards[1].y+st.btn3, 32-st.btn1-boards[1].width);
-        boards[1].y = MAX(boards[1].y-st.btn4, 0);
+            /* Control so that the boards are not going out of the screen */
+            boards[0].y = MIN(boards[0].y+st.btn1, 32-st.btn1-boards[0].width);
+            boards[0].y = MAX(boards[0].y-st.btn2, 0);
+        } else {
+            /* Control so that the boards are not going out of the screen */
+            boards[0].y = MIN(boards[0].y+st.btn3, 32-st.btn3-boards[0].width);
+            boards[0].y = MAX(boards[0].y-st.btn4, 0);
+
+            boards[1].y = MIN(boards[1].y+st.btn1, 32-st.btn1-boards[1].width);
+            boards[1].y = MAX(boards[1].y-st.btn2, 0);
+        }
 
         /* Make sure that the ball is inside of the screen, do not allow overflow */
         ball.y = MIN(ball.y, 30);
