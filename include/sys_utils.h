8cb3215d2ed071a11eb3bcf81dc13420a51493f6
monoamine11231 <monoamine11231@gmail.com>
2023-11-15 12:21:42 +0100

    Euclidean modulo implemented

--- include/sys_utils.h
+++ include/sys_utils.h
@@ -7,6 +7,8 @@
 #define MAX(a, b) (((a) < (b)) ? (b) : (a))
 #define TRIM(x, m) (((x) / (m))*(m))
 
+#define MOD(x, m) ((((x)%(m))+(m))%(m))
+
 #define INTERUPT_QUICKDELAY_VALUE 700000
 
 /* Used for setups only, should not be used for real time delay */
