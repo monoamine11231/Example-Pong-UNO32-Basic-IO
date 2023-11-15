9460c7963791cbbdee200433f42ec8352407bd1c
monoamine11231 <monoamine11231@gmail.com>
2023-11-15 11:27:24 +0100

    tab indentation fixed

--- src/sys/sys_handlers.c
+++ src/sys/sys_handlers.c
@@ -9,20 +9,20 @@ int errno;
 
 /* Non maskable handler */
 void _nmi_handler() {
-    EXIT_LOOP;
+	EXIT_LOOP;
 }
 
 /* Is called before .data and .bss is set up */
 void _on_reset() {
-    /* Nothing */
+	/* Nothing */
 }
 
 /* Called before main, a nice setup can be set up */
 void _on_bootstrap() {
-        /*
-	  This will set the peripheral bus clock to the same frequency
-	  as the sysclock. That means 80 MHz, when the microcontroller
-	  is running at 80 MHz. Changed 2017, as recommended by Axel.
+		/*
+	This will set the peripheral bus clock to the same frequency
+	as the sysclock. That means 80 MHz, when the microcontroller
+	is running at 80 MHz. Changed 2017, as recommended by Axel.
 	*/
 	SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
 	SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
@@ -55,34 +55,34 @@ void _on_bootstrap() {
 	/* SPI2STAT bit SPIROV = 0; */
 	SPI2STATCLR = 0x40;
 	/* SPI2CON bit CKP = 1; */
-        SPI2CONSET = 0x40;
+		SPI2CONSET = 0x40;
 	/* SPI2CON bit MSTEN = 1; */
 	SPI2CONSET = 0x20;
 	/* SPI2CON bit ON = 1; */
 	SPI2CONSET = 0x8000;
 
-    /* INITIALIZE DISPLAY!!!!! */
-    
-    /* Display change to command mode */
-    PORTFCLR = 0x10;
+	/* INITIALIZE DISPLAY!!!!! */
+	
+	/* Display change to command mode */
+	PORTFCLR = 0x10;
 
 	quicksleep(10);
-    /* Display active VDD */
-    PORTFCLR = 0x40;
+	/* Display active VDD */
+	PORTFCLR = 0x40;
 
 	quicksleep(1000000);
 	
 	spi_send_recv(0xAE);
 
-    /* Display activate reset*/
-    PORTGCLR = 0x200;
+	/* Display activate reset*/
+	PORTGCLR = 0x200;
 
 	quicksleep(10);
 	
 	/* Display do not reset */
-    PORTGSET = 0x200;
+	PORTGSET = 0x200;
 
-    quicksleep(10);
+	quicksleep(10);
 	
 	spi_send_recv(0x8D);
 	spi_send_recv(0x14);
@@ -90,8 +90,8 @@ void _on_bootstrap() {
 	spi_send_recv(0xD9);
 	spi_send_recv(0xF1);
 	
-    /* Display activate VBAT */
-    PORTFCLR = 0x20;
+	/* Display activate VBAT */
+	PORTFCLR = 0x20;
 
 	quicksleep(10000000);
 	
@@ -107,7 +107,7 @@ void _on_bootstrap() {
 	spi_send_recv(0xAF);
 	quicksleep(1000);
 
-    /* CLEAR TEXT BUFFER!! */
+	/* CLEAR TEXT BUFFER!! */
 	clear_video();
 	flush_video();
 
