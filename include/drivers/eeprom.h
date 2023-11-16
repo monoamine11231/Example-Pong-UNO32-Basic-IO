74b018efb5d447d0b6b6247ba67029fbae38bcfa
monoamine11231 <monoamine11231@gmail.com>
2023-11-16 21:51:38 +0100

    Idle hang bug fixed + delay after write added

--- include/drivers/eeprom.h
+++ include/drivers/eeprom.h
@@ -1,6 +1,14 @@
 #ifndef INCLUDE_DRIVERS_EEPROM_H
 #define INCLUDE_DRIVERS_EEPROM_H
 
+#define EEPROM_OPERATION_WRITE 0xA0
+#define EEPROM_OPERATION_READ 0xA1 
+
+/* Stops the EEPROM in a valid way by sending a NACK bit, if not done so.
+ * This stop EEPROM from glitching.
+ */
+void eeprom_reset();
+
 void eeprom_dump_struct(const void *src, int st_size, int index);
 void eeprom_read_struct(void *dest, int st_size, int index);
 
--- src/drivers/eeprom.c
+++ src/drivers/eeprom.c
@@ -1,21 +1,25 @@
 #include "drivers/eeprom.h"
 #include "drivers/i2c.h"
 
+#include <pic32mx.h>
 #include <stdint.h>
 
 
-#define EEPROM_OPERATION_WRITE 0xA0
-#define EEPROM_OPERATION_READ 0xA1 
-#define EEPROM_MEMORY_ADD 0x0000
+void eeprom_reset() {
+    i2c_start();
+    i2c_send_not_acknowledge();
+    i2c_stop();
+}
 
 void eeprom_dump_struct(const void *src, int st_size, int index) {
     int acknowledge;
     do {
         i2c_start();
         acknowledge = i2c_send_u8(EEPROM_OPERATION_WRITE);
-    } while(!acknowledge);
-    i2c_send_u8(EEPROM_MEMORY_ADD >> 2);
-    i2c_send_u8(EEPROM_MEMORY_ADD + index*st_size);
+    } while (!acknowledge);
+    
+    i2c_send_u8(0x00);
+    i2c_send_u8(0x00 + index*st_size);
     
     int i;
     for (i = 0; i < st_size; ++i) {
@@ -24,21 +28,32 @@ void eeprom_dump_struct(const void *src, int st_size, int index) {
     }
 
     i2c_stop();
+    /* 5ms delay after EEPROM write cycle is needed as defined in specifications */
+    quicksleep(100000);
 }
 
 void eeprom_read_struct(void *dest, int st_size, int index) {
+    if (st_size <= 0) {
+        return;
+    }
+
     i2c_start();
     i2c_send_u8(EEPROM_OPERATION_WRITE);
-    i2c_send_u8(EEPROM_MEMORY_ADD >> 2);
-    i2c_send_u8(EEPROM_MEMORY_ADD + index*st_size);
+    i2c_send_u8(0x00);
+    i2c_send_u8(0x00 + index*st_size);
     i2c_restart();
     i2c_send_u8(EEPROM_OPERATION_READ);
-    
     int i;
-    for (i = 0; i < st_size; ++i) {
+    for (i = 0; i < st_size-1; ++i) {
         ((uint8_t*)dest)[i] = i2c_receive_u8();
         i2c_send_acknowledge();
     }
 
+    /* On the last received byte it is important to NOT sent
+     * the acknowledge bit, but rather the not acknowledge bit,
+     * otherwise the EEPROM will bug!!!!!!!!!!.
+     */
+    ((uint8_t*)dest)[st_size-1] = i2c_receive_u8();
+    i2c_send_not_acknowledge();
     i2c_stop();
 }
